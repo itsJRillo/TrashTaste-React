d9d25c578657c0223463edfd9227786a0308ddc1
itsJRillo <jmorillolabour@gmail.com>
2022-10-27 18:17:04 +0200

    API Changes

--- src/api/youtube.jsx
+++ src/api/youtube.jsx
@@ -6,7 +6,7 @@ export default axios.create({
     baseURL:"https://www.googleapis.com/youtube/v3",
     params:{
         part:"snippet",
-        maxResults:30,
+        maxResults:15,
         key:KEY
     },
     headers:[]
